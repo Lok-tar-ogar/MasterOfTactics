cd7b288ee3f6ceac55b3d57dee474b7f89bf3de6
Arnold Lee <418586403@qq.com>
2016-06-20 15:31:54 +0800

    ss

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class adv(models.Model):
     title=models.CharField('标题',max_length=50)
     content=models.CharField('内容',max_length=500)
+    act_date=models.DateTimeField('活动结束时间',null=True)
     dim_date=models.DateTimeField('添加时间',auto_now_add=True)
 
     class Meta:
