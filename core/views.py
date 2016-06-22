c42237628241f0b5723afbb7c0fa72b7de643097
dawsonrose <1768250331@qq.com>
2016-06-22 23:48:27 +0800

    compelete video func

--- core/views.py
+++ core/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render
-# from core.models import *
-# from django.http import HttpResponse, HttpResponseRedirect
-# #from PIL import Image,ImageDraw,ImageFont
-# from math import ceil
-# import random
-# import os
-# import io as cStringIO
-# from datetime import datetime
-# import hashlib
-# from django.core.files.storage import default_storage
-# import json
-# import time
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# from django.db import connection
-# from django.utils import timezone
-# try:
-#     from functools import wraps
-# except ImportError:
-#     from django.utils.functional import wraps  # Python 2.4 fallback.
-# from django.utils.decorators import available_attrs
-# import base64
-#
-# import urllib.parse
-# import urllib
-# import sys
-# import http.cookiejar
-# import json
+from core.models import *
+from django.http import HttpResponse, HttpResponseRedirect
+from PIL import Image,ImageDraw,ImageFont
+from math import ceil
+import random
+import os
+import io as cStringIO
+from datetime import datetime
+import hashlib
+from django.core.files.storage import default_storage
+import json
+import time
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import connection
+from django.utils import timezone
+try:
+    from functools import wraps
+except ImportError:
+    from django.utils.functional import wraps  # Python 2.4 fallback.
+from django.utils.decorators import available_attrs
+import base64
+
+import urllib.parse
+import urllib
+import sys
+import http.cookiejar
+import json
 
 
 
