de8a3b7d55011950b9e147e89b4950c0d8c622fa
Arnold0824 <Schwarzenegger@vip.qq.com>
2016-07-05 23:44:39 +0800

    手术

--- core/views.py
+++ core/views.py
@@ -1,45 +1,46 @@
 #encoding:utf-8
 from django.shortcuts import render
-# from core.models import *
-# from django.http import HttpResponse, HttpResponseRedirect
-# from PIL import Image,ImageDraw,ImageFont
-# from math import ceil
-# import random
-# import os
-# import io as cStringIO
-# from datetime import datetime
-# import hashlib
-# from django.core.files.storage import default_storage
-# import json
-# import time
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# from django.db import connection
-# from django.utils import timezone
-# try:
-#     from functools import wraps
-# except ImportError:
-#     from django.utils.functional import wraps  # Python 2.4 fallback.
-# from django.utils.decorators import available_attrs
-# import base64
-#
-# import urllib.parse
-# import urllib
-# import sys
-# import http.cookiejar
-# import json
+from core.models import *
+from django.http import HttpResponse, HttpResponseRedirect
+from math import ceil
+import random
+import os
+import io as cStringIO
+from datetime import datetime
+import hashlib
+from django.core.files.storage import default_storage
+import json
+import time
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import connection
+from django.utils import timezone
+try:
+    from functools import wraps
+except ImportError:
+    from django.utils.functional import wraps  # Python 2.4 fallback.
+from django.utils.decorators import available_attrs
+import base64
+
+import urllib.parse
+import urllib
+import sys
+import http.cookiejar
+import json
 
 
 #
-# def ipfrom(ip):
-#     url = 'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip
-#     result=post(url,{}).decode()
-#     return json.loads(result)
-#
-#
-# def post(url, data):#封装post方法
-#     return urllib.request.urlopen(url, urllib.parse.urlencode(data).encode('utf-8')).read()
+def ipfrom(ip):
+    url = 'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip
+    result=post(url,{}).decode()
+    return json.loads(result)
+
+
+def post(url, data):#封装post方法
+    return urllib.request.urlopen(url, urllib.parse.urlencode(data).encode('utf-8')).read()
 
 def index(request):
-    # dic=ipfrom(request.META['REMOTE_ADDR'])
+    dic=ipfrom(request.META['REMOTE_ADDR'])
+    try:
+        advs=adv.objects.all()[:5]
     return render(request, 'index.html', locals())
 
