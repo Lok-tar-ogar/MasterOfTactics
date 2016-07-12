e406defb173f839ac5d2a1b678b60b0d4c34e6cd
Arnold Lee <418586403@qq.com>
2016-07-12 15:16:19 +0800

    gg

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class adv(models.Model):
     title=models.CharField('标题',max_length=50)
     content=models.CharField('内容',max_length=500)
     language=models.CharField('语言',max_length=500,default='en',choices=(('en','英文'),('rs','俄文'),('fr','法文'),('gr','德文')))
-    act_date=models.DateTimeField('活动结束时间',null=True)
+    act_date=models.DateField('活动结束时间',null=True)
     dim_date=models.DateTimeField('添加时间',auto_now_add=True)
 
     class Meta:
